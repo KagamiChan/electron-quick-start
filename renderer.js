7890f32db3fe4a7e2afcdcf3366e806e2630c307
Kagami <moe.pascal@gmail.com>
2017-05-02 20:03:20 +0800

    better variable naming

--- renderer.js
+++ renderer.js
@@ -12,21 +12,21 @@ const bufferLength = image.toBitmap().length
 console.log('original image', width, height, bufferLength, width * height * 4)
 
 const crop = image.crop({x: 20, y: 20, width: 220, height: 330})
-const { width:width2, height:height2 } = crop.getSize()
+const { width:cropWidth, height:cropHeight } = crop.getSize()
 const cropLength = crop.toBitmap().length
 const _cropLength = crop.getBitmap().length
-console.log('cropped image', width2, height2, cropLength, _cropLength, width2 * height2 * 4)
+console.log('cropped image', cropWidth, cropHeight, cropLength, _cropLength, cropWidth * cropHeight * 4)
 
 writeFile(resolve(__dirname, './crop.png'), crop.toPNG(), e => {if (e) console.error(e)})
 
 const buffer = crop.toBitmap()
 const crop2 = nativeImage.createFromBuffer(buffer, {
-  width: width2,
-  height: height2,
+  width: cropWidth,
+  height: cropHeight,
 })
 const cropLength2 = crop2.toBitmap().length
 const _cropLength2 = crop2.getBitmap().length
-console.log('cropped and restored image', width2, height2, cropLength2, _cropLength2, width2 * height2 * 4)
+console.log('cropped and restored image', cropWidth, cropHeight, cropLength2, _cropLength2, cropWidth * cropHeight * 4)
 
 const pngBuffer = crop2.toPNG()
 writeFile(resolve(__dirname, './crop2.png'), pngBuffer, e => {if (e) console.error(e)})
