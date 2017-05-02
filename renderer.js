a0d1a974ca85a7a7d781ce264e6c0039811f6e3e
Kagami <moe.pascal@gmail.com>
2017-05-02 19:55:49 +0800

    test add createFromBuffer()

--- renderer.js
+++ renderer.js
@@ -7,12 +7,26 @@ const { resolve } = require('path')
 const { writeFile } = require('fs')
 
 const image = nativeImage.createFromPath(resolve(__dirname, './lena_std.png'))
-let { width, height } = image.getSize()
+const { width, height } = image.getSize()
 const bufferLength = image.toBitmap().length
 console.log('original image', width, height, bufferLength, width * height * 4)
 
-const crop = image.crop({x: 20, y: 20, width: 22, height: 33})
-const { width:_width, height:_height } = crop.getSize()
+const crop = image.crop({x: 20, y: 20, width: 220, height: 330})
+const { width:width2, height:height2 } = crop.getSize()
 const cropLength = crop.toBitmap().length
 const _cropLength = crop.getBitmap().length
-console.log('cropped image', _width, _height, cropLength, _cropLength, _width * _height * 4)
+console.log('cropped image', width2, height2, cropLength, _cropLength, width2 * height2 * 4)
+
+writeFile(resolve(__dirname, './crop.png'), crop.toPNG(), e => {if (e) console.error(e)})
+
+const buffer = crop.toBitmap()
+const crop2 = nativeImage.createFromBuffer(buffer, {
+  width: width2,
+  height: height2,
+})
+const cropLength2 = crop2.toBitmap().length
+const _cropLength2 = crop2.getBitmap().length
+console.log('cropped and restored image', width2, height2, cropLength2, _cropLength2, width2 * height2 * 4)
+
+const pngBuffer = crop2.toPNG()
+writeFile(resolve(__dirname, './crop2.png'), pngBuffer, e => {if (e) console.error(e)})
