0f5194d9f2e6466ea14ca7fd9db6f8861c2488d9
Kagami <moe.pascal@gmail.com>
2017-09-01 13:57:54 +0800

    add vibrancy & devtool open

--- renderer.js
+++ renderer.js
@@ -1,3 +1,16 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+const { remote } = require('electron')
+
+const win = remote.getCurrentWindow()
+win.setVibrancy('ultra-dark')
+
+win.webContents.openDevTools({mode: 'detach'})
+
+win.webContents.on('devtools-closed', () => {
+  setTimeout(() => {
+    console.log('restoring vibrancy')
+    win.setVibrancy('ultra-dark')
+  }, 1000)
+})
