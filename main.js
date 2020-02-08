253253be9b381ef24148c6fb194157246c0a2558
Kagami <moe.pascal@gmail.com>
2020-02-08 21:50:15 +0800

    feat: repro cert error crash

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadURL('https://blog.kagami.moe/')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
